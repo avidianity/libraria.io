c7df3b011cf7a04ec073a90694e8f9a3a2c575b6
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2020-11-05 23:17:49 +0800

    Added cloud storage

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Spatie\Permission\PermissionServiceProvider::class,
+        BC\Laravel\DropboxDriver\ServiceProvider::class,
 
         /*
          * Application Service Providers...
